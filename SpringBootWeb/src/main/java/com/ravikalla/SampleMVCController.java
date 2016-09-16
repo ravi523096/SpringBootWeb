765c139200b6d0c59f21d18dfc706d3079d907fd
Ravi Kalla <ravi2523096@gmail.com>
2016-09-16 14:14:44 -0400

    Thyme updates

--- SpringBootWeb/src/main/java/com/ravikalla/SampleMVCController.java
+++ SpringBootWeb/src/main/java/com/ravikalla/SampleMVCController.java
@@ -1,13 +1,19 @@
 package com.ravikalla;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleMVCController {
+
 	@RequestMapping("/hi")
 	public @ResponseBody String hiThere() {
-		return "Hello World";
+		return "Hello World!";
+	}
+	@RequestMapping(value="/hi0/{testStr}")
+	public @ResponseBody String hiThere0(@PathVariable String strName) {
+		return "Hello World0! " + strName;
 	}
 }
