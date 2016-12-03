0a29e88de7455ae3e8d4ed3fe24311370dc7dcd1
Ravi Kalla <ravi2523096@gmail.com>
2016-12-02 23:48:28 -0500

    documents

--- SpringCloudConfigServer/src/main/java/in/ravikalla/SpringCloudConfigServerApplication.java
+++ SpringCloudConfigServer/src/main/java/in/ravikalla/SpringCloudConfigServerApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
+//	http://localhost:8001/SpringCloud-client/default
+//	http://localhost:8001/SpringCloud-client/northamerica
+//	http://localhost:8001/SpringCloud-client/southamerica
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
